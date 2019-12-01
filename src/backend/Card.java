258b42933b5621df7d89149d86ad21fa0305fb36
Daniel Nemeth <nemethdanka@gmail.com>
2019-12-01 18:03:58 +0100

    card class

--- src/backend/Card.java
+++ src/backend/Card.java
@@ -1,4 +1,35 @@
 package backend;
 
-public class Card {
+import java.util.*;
+
+public class Card implements Comparable<Card>{
+    private List<Side> sides;
+    private SortedSet<Challenge> challenges;
+
+    public Card(List<Side> sides){
+        this.sides=sides;
+        for(Side s: sides){
+            Set<Side> ch=new TreeSet<Side>();
+            ch.add(s);
+            Set<Side> rp=new TreeSet<Side>();
+            for(Side s2: sides){
+                if(!(s.equals(s2))) rp.add(s2);
+            }
+
+            challenges.add(new Challenge(this, ch, rp));
+        }
+
+    }
+
+    public Boolean isDue(){
+        return challenges.first().isDue();
+    }
+
+    public Challenge getFirstChallenge(){return challenges.first();}
+
+
+    @Override
+    public int compareTo(Card card) {
+        return challenges.first().compareTo(card.challenges.first());
+    }
 }
